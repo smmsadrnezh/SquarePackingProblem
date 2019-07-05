bdccefab8bcce6c934962bb4cefe4fde350a7355
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2019-07-06 01:32:07 +0430

    Change code structure

---
+++ problem.py
@@ -0,0 +1,28 @@
+class Square:
+    def __init__(self, up, right, down, left):
+        self.dents = {'up': up, 'right': right, 'down': down, 'left': left}
+        self.used = False
+
+    def __repr__(self):
+        return '(u{} r{} d{} l{})'.format(self.dents['up'], self.dents['right'], self.dents['down'], self.dents['left'])
+
+
+def problem():
+    while True:
+        try:
+            n = int(input('Enter n: '))
+            break
+        except ValueError:
+            print('Wrong Number.')
+
+    squares = list()
+    for i in range(n * n):
+        while True:
+            try:
+                edges = input('Enter Dents in Clockwise Order For Square[{}][{}]:\n'.format(i // n, i % n))
+                squares.append(Square(*(int(dent) for dent in edges.split(' '))))
+                break
+            except TypeError:
+                print('Wrong Dents.')
+
+    return n, squares
---
+++ solution.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+def solution(n, squares):
+    big_square = [[-1] * n for _ in range(n)]
+
+    for row in range(n):
+        for column in range(n):
+            edges = dict()
+
+            if row == 0:
+                edges['up'] = 0
+            elif row == n - 1:
+                edges['up'] = - big_square[row - 1][column].dents['down']
+                edges['down'] = 0
+            else:
+                edges['up'] = - big_square[row - 1][column].dents['down']
+
+            if column == 0:
+                edges['left'] = 0
+            elif column == n - 1:
+                edges['left'] = - big_square[row][column - 1].dents['right']
+                edges['right'] = 0
+            else:
+                edges['left'] = - big_square[row][column - 1].dents['right']
+
+            for square in squares:
+                if square.used:
+                    continue
+
+                if square.dents['up'] == edges['up'] and square.dents['left'] == edges['left']:
+                    right = square.dents['right'] == edges['right'] if 'right' in edges.keys() else True
+                    down = square.dents['down'] == edges['down'] if 'down' in edges.keys() else True
+                    if right and down:
+                        square.used = True
+                        big_square[row][column] = square
+                        break
+            else:
+                return 'No Possible Answer For The Given Input'
+
+    return np.matrix(big_square)
